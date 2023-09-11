1ffec5d5d7410ac2be56fc6c9df912d7aabb2e4d
kapilkungwani <kapilkungwani1820@gmail.com>
2023-09-11 20:48:59 +0530

    Add Frontend and Backend minor changes

---
+++ frontend/client/src/App.js
@@ -0,0 +1,14 @@
+// import Home from "./components/Home";
+import "./App.css";
+import FirstPage from "./components/FirstPage";
+
+function App() {
+  return (
+    <div>
+      <FirstPage />
+      {/* <LandingPage/> */}
+    </div>
+  );
+}
+
+export default App;
